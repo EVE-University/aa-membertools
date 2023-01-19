7568bd56ae2898054759176714ffa4768b4b1864
Marn Vermuldir <marn@minin.io>
2023-01-19 02:35:40 -0600

    Added console command to create members from current users

---
+++ membertools/management/commands/membertools_create_members.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand, CommandError
+
+from membertools.models import Member, Character
+
+from membertools.app_settings import MEMBERTOOLS_MAIN_CORP_ID
+
+
+class Command(BaseCommand):
+    help = "Initializes the Member with Main Characters that are in the Main Corp"
+
+    def add_arguments(self, parser) -> None:
+        parser.add_argument("--main_corp_id", type=int, required=False)
+
+    def handle(self, *args, **options):
+        main_corp = options["main_corp_id"]
+        if main_corp is None:
+            main_corp = MEMBERTOOLS_MAIN_CORP_ID
+
+        query = User.objects.select_related("profile__main_character").filter(
+            profile__main_character__corporation_id=main_corp
+        )
+
+        for user in query:
+            try:
+                member = Member.objects.get(
+                    main_character__character_ownership__user=user
+                )
+            except Member.DoesNotExist:
+                member = Member.objects.create(
+                    main_character=user.profile.main_character,
+                    first_main_character=user.profile.main_character,
+                )
+
+            char_query = Character.objects.filter(
+                eve_character__character_ownership__user=user
+            )
+            for char in char_query:
+                char.member = member
+                char.save()
