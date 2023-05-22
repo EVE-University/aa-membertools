f53c98d0010247cedb1ee02946842eb6f335636b
Marn Vermuldir <marn@minin.io>
2023-05-21 20:04:29 -0500

    Added memberaudit as a dep and put its required apps in test settings to fix tests

--- testauth/settings.py
+++ testauth/settings.py
@@ -259,7 +259,7 @@ SITE_NAME = "testauth"
 DEBUG = False
 
 # Add any additional apps to this list.
-INSTALLED_APPS += ["membertools"]
+INSTALLED_APPS += ["eveuniverse", "memberaudit", "membertools"]
 
 # Register an application at https://developers.eveonline.com for Authentication
 # & API Access and fill out these settings. Be sure to set the callback URL
